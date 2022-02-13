aea9c5e8f3209a500c5f3d177b166d5016cb44fd
coG9 <williamcolinsgeorge@gmail.com>
2022-02-13 17:32:56 -0500

    none

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var jwt = require('jsonwebtoken')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const cors = require('cors')
-
+const bodyParser = require('body-parser')
 
 
 const mongoose = require('mongoose')
@@ -27,6 +27,8 @@ app.set('view engine', 'ejs');
 
 app.use(cors())
 app.use(logger('dev'));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
