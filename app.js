ff3385f9a91802e8fb2fa2696cdd68c7dbf49fbe
coG9 <williamcolinsgeorge@gmail.com>
2022-02-27 17:55:09 -0500

    none

--- app.js
+++ app.js
@@ -27,7 +27,9 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(cors())
+app.use(cors({
+  origin: "*",
+}))
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
